13314785393d121c7754cf70979fb70bfca68779
Shea Challis <shea.challis@hotmail.co.uk>
2023-04-09 17:11:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import ToDoList from "./components/ToDoList";
+import { v4 as uuidv4 } from "uuid";
+
+const LOCAL_STORAGE_KEY = "toDoApp.toDos";
 
 function App() {
+  const [toDos, setToDos] = useState([]);
+  const toDoNameRef = useRef();
+
+  useEffect(() => {
+    const storedToDos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedToDos) setToDos(storedToDos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(toDos));
+  }, [toDos]);
+
+  function toggleToDo(id) {
+    const newToDos = [...toDos];
+    const toDo = newToDos.find((toDo) => toDo.id === id);
+    toDo.complete = !toDo.complete;
+    setToDos(newToDos);
+  }
+
+  function handleAddToDo(e) {
+    const name = toDoNameRef.current.value;
+    if (name === "") return;
+    setToDos((prevToDos) => {
+      return [...prevToDos, { id: uuidv4(), name: name, complete: false }];
+    });
+    toDoNameRef.current.value = null;
+  }
+
+  function handleClearToDos() {
+    const newToDos = toDos.filter((toDo) => !toDo.complete);
+    setToDos(newToDos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input ref={toDoNameRef} type="text" />
+      <button onClick={handleAddToDo}>Add To-Do</button>
+      <ToDoList toDos={toDos} toggleToDo={toggleToDo} />
+      <button onClick={handleClearToDos}>Clear Completed</button>
+      <div>{toDos.filter((toDo) => !toDo.complete).length} left to do</div>
+    </>
   );
 }
 
---
+++ src/components/ToDoList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ToDo from "./ToDo";
+
+export default function ToDoList({ toDos, toggleToDo }) {
+  return toDos.map((toDo) => {
+    return <ToDo key={toDo.id} toggleToDo={toggleToDo} toDo={toDo} />;
+  });
+}
